ff079670f56358c1438e2d33846234b44598be53
Md Al Amin <alaminislam51087@gmail.com>
2026-09-13 10:57:30 +0600

    Update Supabase key in script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = https://jqpxkqitklzxhzcobisv.supabase.co/rest/v1/
 
-const SUPABASE_KEY = "এখানে আপনার Publishable Key বসাবেন";
+const SUPABASE_KEY = sb_publishable_sF42Wa1UmTXPOXK9NvMVdw_eZiKyh6o
