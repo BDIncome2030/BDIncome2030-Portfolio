f2a09b98141cb8fc11f0cb0727f3470aa06d7185
Md Al Amin <alaminislam51087@gmail.com>
2026-09-13 11:01:01 +0600

    Fix SUPABASE_URL format in script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = https://jqpxkqitklzxhzcobisv.supabase.co/rest/v1/
+const SUPABASE_URL = https://jqpxkqitklzxhzcobisv.supabase.co
 
 const SUPABASE_KEY = sb_publishable_sF42Wa1UmTXPOXK9NvMVdw_eZiKyh6o
